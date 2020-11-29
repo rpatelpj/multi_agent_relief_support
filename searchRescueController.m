d88500c6593c28f65735237df4843f7b9d7797c0
atrimper <liv4waterville@gmail.com>
2020-11-29 17:52:54 -0500

    Added gradient descent controller in searchRescueController. Modified parameters in main.

--- searchRescueController.m
+++ searchRescueController.m
@@ -1,6 +1,22 @@
 %% Search and Rescue Controller
-function dxi = searchRescueController(x, map, agentIdxRadius)
+function dxi = searchRescueController(x, map, agentIdxRadius, metricToIdx, fieldSize)
+    mapSize = size(map);
+    xInd = (metricToIdx .* (x - [fieldSize(1); fieldSize(3)])) + 1;
+    dxi = zeros(size(x));
+    for agent = 1:length(x)
+        % TODO: visibility disc currently a square, change to disc
+        lowerXBound = floor(max((xInd(1, agent) - agentIdxRadius), 1));
+        upperXBound = ceil(min((xInd(1, agent) + agentIdxRadius), mapSize(2)));
+        lowerYBound = floor(max((xInd(2, agent) - agentIdxRadius), 1));
+        upperYBound = ceil(min((xInd(2, agent) + agentIdxRadius), mapSize(1)));
 
-    % TODO
+        visibleMap = map(lowerYBound:upperYBound, lowerXBound:upperXBound);
+        visibleXGrad = visibleMap(:, 1:end-1) - visibleMap(:, 2: end);
+        visibleYGrad = visibleMap(1:end-1, :) - visibleMap(2: end, :);
 
+        if any(any(visibleMap ~= 0))
+            dxi(:, agent) = [mean(mean(visibleXGrad(visibleXGrad ~= 0)));
+                             mean(mean(visibleYGrad(visibleYGrad ~= 0)))];
+        end
+    end
 end
