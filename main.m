43d306b2cbbe977775b8c509e5f02b93305571a2
Raj Patel <rpatelpj@gmail.com>
2020-11-30 19:58:54 -0500

    Refine state machine in controller

--- main.m
+++ main.m
@@ -11,11 +11,11 @@ agentMetricVisibleApothem = 0.1; % Metric Radius of Visible Map of Agent
 
 % Simulation
 contourRes = 5; % Contour Resolution
-iteration = 500; % Total Number of Iterations
+iteration = 2000; % Total Number of Iterations
 
 % Map
-numSink = 6; % Number of Sinks
-sinkMetricLen = 0.4; % Metric Length of Square Sink
+numSink = 5; % Number of Sinks
+sinkMetricLen = 0.7; % Metric Length of Square Sink
 sinkIdxLen = 11; % Index Length of Square Sink
 sinkDepth = 1.5; % Depth of Sink
 
@@ -30,7 +30,7 @@ agentIdxVisibleApothem = floor(agentMetricVisibleApothem.*metricToIdx) + 1; % In
 %% Run Driver with Robotarium
 
 roboDrv = Robotarium('NumberOfRobots', numAgent, 'InitialConditions', agentMetricPos0);
-agentMetricPos = roboDrv.get_poses();
+agentMetricPosu = roboDrv.get_poses();
 roboDrv.step(); % Set agents' initial pose
 siToUni = create_si_to_uni_dynamics();
 uniClamp = create_uni_barrier_certificate_with_boundary();
@@ -38,22 +38,22 @@ uniClamp = create_uni_barrier_certificate_with_boundary();
 contour(xMapMetricGrid, yMapMetricGrid, map, contourRes); % Plot contour of map
 
 for k = 1:iteration
-    agentMetricPos = roboDrv.get_poses();
-    agentMetricPosi = agentMetricPos(1:2, :); % Get agents' single integrator position
+    agentMetricPosu = roboDrv.get_poses();
+    agentMetricPosi = agentMetricPosu(1:2, :); % Get agents' single integrator position
     agentMetricVeli = zeros(2, numAgent); % Initialize agents' single integrator velocities
 
     for agentN = 1:numAgent
         agentNMetricPosi = agentMetricPosi(1:2, agentN);
         agentAdjacentN = find(A(:, agentN) == 1); % Get set of agents adjacent to agent N
         visibleMapN = readSensorSim(agentNMetricPosi, agentIdxVisibleApothem, map, fieldDim(1), fieldDim(3), metricToIdx); % Extract visible disk of agent
-        [agentMetricVelNi, agentNState] = searchRescueController(agentN, agentAdjacentN, visibleMapN, agentMetricPosi, agentState); % Execute controller
+        [agentMetricVelNi, agentNState] = searchRescueController(agentN, agentAdjacentN, visibleMapN, agentMetricPosi, agentState, sinkMetricLen); % Execute controller
         agentMetricVeli(:, agentN) = agentMetricVelNi;
         agentState(agentN) = agentNState;
     end
 
-    dxu = siToUni(agentMetricVeli, agentMetricPos); % Convert single integrator to unicycle
-    dxu = uniClamp(dxu, agentMetricPos); % Impose inter-agent barrier and field boundary
-    roboDrv.set_velocities(1:numAgent, dxu);
+    agentMetricVelu = siToUni(agentMetricVeli, agentMetricPosu); % Convert single integrator to unicycle
+    agentMetricVelu = uniClamp(agentMetricVelu, agentMetricPosu); % Impose inter-agent barrier and field boundary
+    roboDrv.set_velocities(1:numAgent, agentMetricVelu);
     roboDrv.step();
 end
 
--- searchRescueController.m
+++ searchRescueController.m
@@ -1,31 +1,76 @@
 %% Search and Rescue Controller
-function [agentMetricVelNi, agentNState] = searchRescueController(agentN, agentAdjacentN, visibleMapN, agentMetricPosi, agentState)
+function [agentMetricVelNi, agentNState] = searchRescueController(agentN, agentAdjacentN, visibleMapN, agentMetricPosi, agentState, sinkMetricLen)
     % Calculate gradient of visible map of agent
+    % TODO: start with metric gradient then convert to index gradient
     visibleMapN = flipud(visibleMapN); % Transform visible map of agent to (-y, x) from (y, x)
     xGradVisibleMapN = -diff(visibleMapN, 1, 2);
     yGradVisibleMapN = -diff(visibleMapN, 1, 1);
     yGradVisibleMapN = flipud(yGradVisibleMapN); % Transform y gradient of visible map of agent to (y, x) from (-y, x)
 
-    % State B: descend sink
-    % TODO: Make sure agent doesn't go into same sink as another agent. Need to restrict state B for that effect.
-    if any((visibleMapN ~= 0), 'all')
-        agentNState = 1;
-        agentMetricVelNi = [mean(xGradVisibleMapN(xGradVisibleMapN ~= 0), 'all');
-                            mean(yGradVisibleMapN(yGradVisibleMapN ~= 0), 'all')];
-    % State A: search for sink
+    if (agentState(:, agentN) ~= 2)
+        if (agentState(:, agentN) == 1)
+            agentNState = 1;
+            agentMetricVelNi = [mean(xGradVisibleMapN(xGradVisibleMapN ~= 0), 'all');
+                                mean(yGradVisibleMapN(yGradVisibleMapN ~= 0), 'all')];
+            if agentMetricVelNi < [10.^(-5), 10.^(-5)]
+                agentNState = 2;
+            end
+        elseif any((visibleMapN ~= 0), 'all')
+            agentStateBSet = find(agentState ~= 0);
+            sinkKnownSet = intersect(agentStateBSet, agentAdjacentN);
+            agentAdjacentNDist = sort(vecnorm(agentMetricPosi(:, sinkKnownSet) - agentMetricPosi(:, agentN)));
+            if isempty(sinkKnownSet)
+                agentNState = 1;
+                agentMetricVelNi = [mean(xGradVisibleMapN(xGradVisibleMapN ~= 0), 'all');
+                                    mean(yGradVisibleMapN(yGradVisibleMapN ~= 0), 'all')];
+            elseif (agentAdjacentNDist(:, 1) > sinkMetricLen.*sqrt(2)) % TODO: maybe not sufficient condition
+                agentNState = 1;
+                agentMetricVelNi = [mean(xGradVisibleMapN(xGradVisibleMapN ~= 0), 'all');
+                                    mean(yGradVisibleMapN(yGradVisibleMapN ~= 0), 'all')];
+            else
+                agentNState = 0;
+                agentMetricVelNi = [0.2; 0.2]; % TODO: allow more mobility
+    %             agentMetricVelNi = zeros(2);
+    %             for sinkKnown = sinkKnownSet'
+    %                 agentMetricVelNi = agentMetricPosi(:, agentN) - agentMetricPosi(:, sinkKnown);
+    %             end
+    %             if ((agentMetricPosi(1, agentN) > 1.5) && (agentMetricPosi(2, agentN) > 0.8))
+    %                 agentMetricVelNi = [-2; -2];
+    %             elseif ((agentMetricPosi(1, agentN) < -1.5) && (agentMetricPosi(2, agentN) < -0.8))
+    %                 agentMetricVelNi = [2; 2];
+    %             elseif ((agentMetricPosi(1, agentN) > 1.5) && (agentMetricPosi(2, agentN) < -0.8))
+    %                 agentMetricVelNi = [-2; 2];
+    %             elseif ((agentMetricPosi(1, agentN) < -1.5) && (agentMetricPosi(2, agentN) > 0.8))
+    %                 agentMetricVelNi = [2; -2];
+    %             end
+            end
+        else
+            agentNState = 0;
+            agentMetricVelNi = [0.2; 0.2]; % TODO: allow more mobility
+    %         agentStateBSet = find(agentState ~= 0);
+    %         sinkKnownSet = intersect(agentStateBSet, agentAdjacentN);
+    %         if isempty(sinkKnownSet)
+    %             agentNState = 0;
+    %             agentMetricVelNi = [0.2; 0.2];
+    %         else
+    %             agentNState = 0;
+    %             agentMetricVelNi = zeros(2);
+    %             for sinkKnown = sinkKnownSet'
+    %                 agentMetricVelNi = agentMetricPosi(:, agentN) - agentMetricPosi(:, sinkKnown);
+    %             end
+    %         end
+    %         if ((agentMetricPosi(1, agentN) > 1.5) && (agentMetricPosi(2, agentN) > 0.8))
+    %             agentMetricVelNi = [-2; -2];
+    %         elseif ((agentMetricPosi(1, agentN) < -1.5) && (agentMetricPosi(2, agentN) < -0.8))
+    %             agentMetricVelNi = [2; 2];
+    %         elseif ((agentMetricPosi(1, agentN) > 1.5) && (agentMetricPosi(2, agentN) < -0.8))
+    %             agentMetricVelNi = [-2; 2];
+    %         elseif ((agentMetricPosi(1, agentN) < -1.5) && (agentMetricPosi(2, agentN) > 0.8))
+    %             agentMetricVelNi = [2; -2];
+    %         end
+        end
     else
         agentNState = 0;
-        agentStateBSet = find(any(agentState == 1, 'all'));
-        sinkKnownSet = intersect(agentStateBSet, agentAdjacentN);
-        % If no sink is found, move randomly
-        if isempty(sinkKnownSet)
-            agentMetricVelNi = [0.2; 0.2]; % TODO: Find better way to initially move.
-        % If sink is found, avoid it
-        else
-              agentMetricVelNi = zeros(2);
-              for sinkKnown = sinkKnownSet
-                  agentMetricVelNi = agentMetricPosi(:, sinkKnown) - agentMetricPosi(:, agentN);
-              end
-        end
+        agentMetricVelNi = [0; 0];
     end
 end
